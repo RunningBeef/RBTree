d19eeb443bb00d5d9772dbb92bc98404000fe3d9
RunningBeef <2723772192@qq.com>
2022-10-02 13:03:01 +0800

    test RedBlackTree performance

--- src/testRedBlackTree.cc
+++ src/testRedBlackTree.cc
@@ -1,5 +1,7 @@
 #include "../include/RedBlackTree.hpp"
-
+#include <map>
+#include <ctime>
+// 数据量较小情况下红黑树是否正常，并且打印测试每一步树的结构
 void testSmallData(int n)
 {
     RedBlackTree<int, int> red_black_tree;
@@ -43,6 +45,7 @@ void testSmallData(int n)
     cout << "RedBlackTree test success!!!" << endl;
 }
 
+// 数据量较大情况下红黑树是否正常
 void testBigData(int n)
 {
     RedBlackTree<int, int> red_black_tree;
@@ -64,7 +67,7 @@ void testBigData(int n)
         red_black_tree.insert(key, 1);
         red_black_tree.checkRedBlackTree();
     }
-
+    cout<< "insert ok!" << endl;
     cout << "-------------------------------------------------------------------------------" << endl;
     cout << "-------------------------------------------------------------------------------" << endl;
 
@@ -80,8 +83,62 @@ void testBigData(int n)
         red_black_tree.erase(key);
         red_black_tree.checkRedBlackTree();
     }
-
+    cout << "erase ok!" << endl;
     cout << "RedBlackTree test success!!!" << endl;
+}
+
+template <class T1, class T2>
+void testRedBlackTreeInsert(int n, RedBlackTree<T1, T2> &redBlackTree)
+{
+    clock_t begin = clock();
+    while (n--)
+        redBlackTree.insert(n, 1);
+    clock_t end = clock();
+    cout << "RedBlackTree insert cost: " << end - begin << endl;
+}
+
+template <class T1, class T2>
+void testRedBlackTreeErase(int n, RedBlackTree<T1, T2> &redBlackTree)
+{
+    clock_t begin = clock();
+    while (n--)
+        redBlackTree.erase(n);
+    clock_t end = clock();
+    cout << "RedBlackTree erase cost: " << end - begin << endl;
+}
+
+void testMapInsert(int n, map<int, int> &mp)
+{
+    clock_t begin = clock();
+    while (n--)
+        mp[n] = 1;
+    clock_t end = clock();
+    cout << "map insert cost: " << end - begin << endl;
+}
+
+void testMapErase(int n, map<int, int> &mp)
+{
+    clock_t begin = clock();
+    while (n--)
+        mp.erase(mp.find(n));
+    clock_t end = clock();
+    cout << "map erase cost: " << end - begin << endl;
+}
+
+// 通过对比map的插入删除时间消耗，判断红黑树性能是否正常
+void testTimeCost()
+{
+    cout << "\n\n\n--------------------compare time cost with map------------------" << endl;
+    int volume = 1e5;
+    map<int, int> mp;
+    RedBlackTree<int, int> redBlackTree;
+
+    testRedBlackTreeInsert(volume, redBlackTree);
+    testRedBlackTreeErase(volume, redBlackTree);
+
+    testMapInsert(volume, mp);
+    testMapErase(volume, mp);
+    return;
 }
 
 int main()
@@ -90,8 +147,9 @@ int main()
     cout << "Please enter a non negative integer < 50 to test the accuracy " << endl;
     cin >> n;
     testSmallData(n);
-    cout << "Please enter a non negative integer < 1000000 to test the performance" << endl;
+    cout << "\n\n\nPlease enter a non negative integer < 1000000 to test the performance" << endl;
     cin >> n;
     testBigData(n);
+    testTimeCost();
     return 0;
 }
